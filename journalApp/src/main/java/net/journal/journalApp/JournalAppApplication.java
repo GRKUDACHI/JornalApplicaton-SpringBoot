f0a1832deadfc8cdd1b8ca68af66c62d08599a5f
Girish_k <girish.k@tya.co.in>
2025-05-22 16:17:03 +0530

    Add new note Abour Spring Annotation

--- journalApp/src/main/java/net/journal/journalApp/JournalAppApplication.java
+++ journalApp/src/main/java/net/journal/journalApp/JournalAppApplication.java
@@ -2,7 +2,7 @@ package net.journal.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Annotation @SpringBootApplication its manages 3 annonation 1)Configuration 2)EnableAutoConfiguration, 3)ComponentScan
 @SpringBootApplication
 public class JournalAppApplication
 {
